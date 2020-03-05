0950155ff4586a925118a89a875aa33d649b5875
esautorres <esau.torres@kellogg.northwestern.edu>
2020-03-05 01:00:56 +0000

    MVP in-class example

--- app.rb
+++ app.rb
@@ -46,6 +46,16 @@ end
 get "/events/:id/rsvps/create" do
     puts "params: #{params}"
 
+    # first find the event that rsvp'ing for 
+    @event = events_table.where(id: params[:id]).to_a[0]
+    # next we want to insert a row in the rsvp 
+    rsvps_table.insert(
+        event_id:@event[:id],
+        name: params["name"],
+        email: params["email"], 
+        comments: params["comments"], 
+        going: params["going"]
+    )
     view "create_rsvp"
 end
 
